4ffbd2f6970cd9e23adb009a2cd3bc052116cad2
zurdi <zurdizurdo25@gmail.com>
2025-01-23 15:59:08 +0000

    added glyph

--- RomM.sh
+++ RomM.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 # HELP: RomM muOS client to download roms directly from your RomM server
+# ICON: romm
 
 . /opt/muos/script/var/func.sh
 
 echo app >/tmp/act_go
+
+ROOT_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/application/.romm"
+LOG_DIR="${ROOT_DIR}/logs"
+mkdir -p "${LOG_DIR}"
+
+# Check if romm.png exists
+if ! -f /opt/muos/default/MUOS/theme/active/glyph/muxapp/romm.png
+then
+    cp "${ROOT_DIR}/resources/romm.png" "/opt/muos/default/MUOS/theme/active/glyph/muxapp/romm.png"
+fi
 
 # Check if pip is installed
 if ! command -v pip3 &> /dev/null
@@ -23,9 +34,6 @@ then
     pip install python-dotenv
 fi
 
-ROOT_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/application/.romm"
-LOG_DIR="${ROOT_DIR}/logs"
-mkdir -p "${LOG_DIR}"
 
 cd "${ROOT_DIR}" || exit
 
