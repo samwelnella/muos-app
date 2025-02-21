848264729aa2cdae7f704ccd015f481c7c46f2a8
zurdi <zurdizurdo25@gmail.com>
2025-02-21 16:50:59 +0000

    debug: logs in the workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ GLYPH_DIR=opt/muos/default/MUOS/theme/active/glyph/muxapp
 FONTS_FIR=usr/share/fonts/romm
 ZIP_BASE_NAME=romm_muOS_install
 VERSION=$(grep -oP '(?<=version = ")[^"]*' .romm/__version__.py)
+echo "Extracted version: $VERSION"
 
 mkdir -p .dist
 mkdir -p ".build/${APPLICATION_DIR}"
@@ -18,5 +19,7 @@ mkdir -p ".build/${FONTS_FIR}"
 cp .romm/fonts/romm.ttf ".build/${FONTS_FIR}"
 
 (cd .build && zip -r "../${ZIP_BASE_NAME}_${VERSION}.zip" *)
+ls -lah  # Debug: Check if ZIP exists in the root
 mv "${ZIP_BASE_NAME}_${VERSION}.zip" ".dist/${ZIP_BASE_NAME}_${VERSION}.zip"
+ls -lah .dist  # Debug: Ensure the file was moved correctly
 rm -rf .build
